00e150c0a7569d3271ba1305991e2286b42d29e5
Ngoga Innocent <ngogainnocent1@gmail.com>
2023-04-24 23:31:27 -0700

    screens folder removed

--- screens/ConsultationScreens/AIScreen/AIstack.js
+++
@@ -1,8 +0,0 @@
-import { View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
-import Aiconsultation from './Aiconsultation'
-import Icon from 'react-native-vector-icons/AntDesign'
-import { COLORS } from '../../../components/Colors'
-
-
--- screens/index.js
+++
@@ -1,1 +0,0 @@
-export {default as OnboadingScreen } from './Onboading'
